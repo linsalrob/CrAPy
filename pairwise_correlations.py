a2d7e7aa45dd534ce0803f729fc0b27948052b3b
linsalrob <raedwards@gmail.com>
2015-08-17 13:56:20 -0700

    limiting to contigs with more than noc occurrences

--- pairwise_correlations.py
+++ pairwise_correlations.py
@@ -35,8 +35,13 @@ if __name__ == '__main__':
     #data_file = "test_data/output.contigs2reads.contigs.100.txt"
     try:
         data_file = sys.argv[1]
+        noc = float(sys.argv[2])
     except:
-        sys.exit(sys.argv[0] + " <data file e.g. test_data/output.contigs2reads.contigs.100.txt>")
+        sys.exit(sys.argv[0] + " <data file e.g. test_data/output.contigs2reads.contigs.100.txt> <minimum number of occurences (try 3)>")
+
+    if noc < 3:
+        sys.stderr.write("Warning: you choose reads that have at least " + str(noc) + " occurrences, but this might be too low. Continuing anyway\n")
+
 
     data = {}
     with open(data_file, 'r') as fin:
@@ -44,6 +49,9 @@ if __name__ == '__main__':
             p=l.strip().split("\t")
             did = p.pop(0)
             p = map(float, p)
+            # only keep those that have > 3 zeros
+            if len(p) - p.count(0) <= noc:
+                continue
             data[did] = p
 
     # now we need to find all pairwise correlation scores
